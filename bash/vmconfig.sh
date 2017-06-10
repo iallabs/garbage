bca1c320c0d61bf1e8849f27a1b666572c60dee4
Hilaly <hilalyamine@gmail.com>
2017-06-10 14:43:14 +0200

    Create vmconfig.sh

--- bash/vmconfig.sh
+++ bash/vmconfig.sh
@@ -1,4 +1,16 @@
 #virtualmachineconfig
 
+# yum&phphttp
 sudo yum -y update
 sudo yum install -y httpd24 php56 php56-mysqlnd
+
+# homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bash_profile
+
+# python
+brew install python
+brew install python3
+brew install django
+brew install sympy
