3f5e919255fbd0597d81233ca7112a5d25a3089a
Hilaly <hilalyamine@gmail.com>
2017-06-23 15:08:55 +0200

    add

--- ial/oasys.sh
+++ ial/oasys.sh
@@ -17,7 +17,7 @@ order () {
     if [ $1='Test Hello']; then
 
         echo 'helloworldtest' > /home/ubuntu/answer/hellotest.txt
+    else
+        echo $($1) > /home/ubuntu/answer/answer.txt
     fi
-
-    echo $($1) > /home/ubuntu/answer/answer.txt
 }
