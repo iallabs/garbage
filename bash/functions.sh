a89eef79a031d3737e67567d7e1d370165e3f35a
Hilaly <hilalyamine@gmail.com>
2017-06-25 18:14:23 +0200

    Update functions.sh

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,3 +1,5 @@
+# function command line options integration
+
 optionsexample () {
     newfile=""
     path=""
@@ -38,3 +40,22 @@ optionsexample () {
         pretty_json "$path/$file"
     fi
 }
+
+# ask yes no example
+
+mkinstances () {
+    source json_utils.sh
+    f=find / -name instances.json
+    if [ -z $f ]; then
+        while true; do
+            read -p "Do you wish to create instances.json file ?" yn
+            case $yn in
+                [Yy]* ) json -n instances -p $default -t list; break;;
+                [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+    esac
+done
+    fi
+    echo "instances.json exit at $f"
+}
+
